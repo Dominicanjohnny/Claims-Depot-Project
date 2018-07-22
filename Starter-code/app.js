02e9fb1033ac3cd01b04e2e7b4410d355753479c
Johnatan <johnatan@johnatans-MacBook-Air-2.local>
2018-07-21 23:41:49 -0400

    newclaim changes

--- Starter-code/app.js
+++ Starter-code/app.js
@@ -37,17 +37,19 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
+app.listen(3000, function () {
+  console.log('Listening on http://localhost:3000/');
+});
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Claims-Depot';
 
 
 
